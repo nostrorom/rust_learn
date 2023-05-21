8157d87a2a3c3421bd812918d1463378148a3e17
nostro <49858211+Nostrorom@users.noreply.github.com>
2023-05-21 15:14:28 +0200

    book 4.1

--- book_4_1/src/main.rs
+++ book_4_1/src/main.rs
@@ -11,9 +11,17 @@ fn main() {
     let mut s3 = String::from("hello");
     change(&mut s3);
 
-    println!("{} : length is {}", s1, len1);
-    println!("{} : length is {}", s2, len2);
-    println!("changed string is {}", s3);
+    println!("{}    --- length {}", s1, len1);
+    println!("{} --- length {}", s2, len2);
+    println!("changed string is --- {}", s3);
+
+    let s = String::from("ciaoooooo mondo");
+
+    println!(
+        "first word: {} - length {}",
+        first_word(&s),
+        first_word(&s).len()
+    );
 }
 
 fn read(y: bool) {
@@ -28,4 +36,16 @@ fn calculate_length(s: &String) -> usize {
 
 fn change(s: &mut String) -> () {
     s.push_str(" world");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes: &[u8] = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
