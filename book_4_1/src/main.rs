8c525fae5ba7b5135beda7550ecc40031297944a
nostro <49858211+Nostrorom@users.noreply.github.com>
2023-05-21 14:04:10 +0200

    book 4.1

--- book_4_1/src/main.rs
+++ book_4_1/src/main.rs
@@ -3,13 +3,17 @@ fn main() {
     read(x);
 
     let s1 = String::from("hello");
-    let s2 = String::from("hello !!");
-
     let len1 = calculate_length(&s1);
+
+    let s2 = String::from("hello !!");
     let len2 = calculate_length(&s2);
+
+    let mut s3 = String::from("hello");
+    change(&mut s3);
 
     println!("{} : length is {}", s1, len1);
     println!("{} : length is {}", s2, len2);
+    println!("changed string is {}", s3);
 }
 
 fn read(y: bool) {
@@ -20,4 +24,8 @@ fn read(y: bool) {
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) -> () {
+    s.push_str(" world");
 }
